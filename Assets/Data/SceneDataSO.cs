f6d570c506e8d928e90b14a1d1d927ec738cfff2
Meghna <bilimoriameghna12@gmail.com>
2021-03-23 00:34:46 -0400

    final version commit

---
+++ Assets/Data/SceneDataSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SceneData", menuName = "Data/SceneData")]
+public class SceneDataSO : ScriptableObject
+{
+   public Vector3 playerPosition;
+    public int playerHealth;
+}
