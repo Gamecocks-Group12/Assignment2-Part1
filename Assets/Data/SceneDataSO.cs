b0a5bdb5c42592b938661e12cdeee9c674ca2cd6
Ramandeep <rmnkaul979697@gmail.com>
2021-04-02 20:05:40 -0400

    fixed the saving AND loading between app loads

--- Assets/Data/SceneDataSO.cs
+++ Assets/Data/SceneDataSO.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "SceneData", menuName = "Data/SceneData")]
+[System.Serializable]
 public class SceneDataSO : ScriptableObject
 {
    public Vector3 playerPosition;
+    public Quaternion playerRotation;
     public int playerHealth;
 }
