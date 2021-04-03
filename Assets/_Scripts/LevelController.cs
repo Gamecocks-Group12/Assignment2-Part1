37eb128bbc5b0699354667c263eb28745e2a592e
Ramandeep <rmnkaul979697@gmail.com>
2021-04-02 22:27:49 -0400

    created pathways for the enemies

--- Assets/_Scripts/LevelController.cs
+++ Assets/_Scripts/LevelController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class LevelController : MonoBehaviour
 {
@@ -11,16 +12,19 @@ public class LevelController : MonoBehaviour
     public List<GameObject> activeTiles;
     public GameObject startTile;
 
-     void Awake()
+    [Header("Navigation")] 
+    private NavMeshSurface surface;
+
+    void Awake()
     {
-        //surface = GetComponent<NavMeshSurface>();
-        //BuildWorld();
+        surface = GetComponent<NavMeshSurface>();
+        BuildWorld();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-       // surface.BuildNavMesh();
+        surface.BuildNavMesh();
     }
 
     // Update is called once per frame
@@ -29,4 +33,24 @@ public class LevelController : MonoBehaviour
         
     }
 
+    private void BuildWorld()
+    {
+        for (int width = 0; width < tileWidth; width++)
+        {
+            for (int length = 0; length < tileLength; length++)
+            {
+                if (activeTiles.Count < 1)
+                {
+                    activeTiles.Add(startTile);
+                    continue;
+                }
+                var randomTileIndex = Random.Range(0, tilePrefabs.Count);
+                var randomTilePosition = new Vector3(width * 16, 0.0f, length * 16);
+                var randomTileRotation = Random.Range(0, 4) * 90.0f;
+                var randomTile = Instantiate(tilePrefabs[randomTileIndex], randomTilePosition, Quaternion.Euler(0.0f, randomTileRotation, 0.0f));
+                randomTile.transform.parent = this.transform; // make this GameObject the Level Parent
+                activeTiles.Add(randomTile);
+            }
+        }
+    }
 }
